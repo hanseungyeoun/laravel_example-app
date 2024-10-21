1eaa05d207043c08e016c34b600c81974651907e
hanseungyeoun <aksla1122@naver.com>
2024-10-21 14:10:10 +0900

    글쓰기 화면 생성

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('root');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/articles/create', function () {
+    return view('articles/create');
+});
+
+require __DIR__ . '/auth.php';
