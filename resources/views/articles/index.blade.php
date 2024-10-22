2f15586637c391bcf2675c807eb6787f9e5e5f30
hanseungyeoun <aksla1122@naver.com>
2024-10-22 13:53:32 +0900

    @foreach 적용

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -11,12 +11,12 @@
 <body>
   <div class="container p-5">
     <h1 class="text-2xl mb-5" >글쓰기</h1>
-    <?php foreach ($articles as $article): ?>
+    @foreach ($articles as $article)
       <div class="background-white border rounded mb-3 p-3">
         <p>{{$article->body}}}</p>
         <p>{{$article->created_at}}</p>
       </div>
-    <?php endforeach;?>
+    @endforeach
   </div>
 </body>
 
